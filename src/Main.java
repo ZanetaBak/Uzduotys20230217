1234d9791f8affe30a0e769c50588149f4bb3ef2
ZanetaB <zzanule@gmail.com>
2023-02-17 13:40:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,108 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+// ==============1 uzduotis =========================
+
+        System.out.println("==================== 1 užduotis ====================");
+        // sukurti cikla, kuris spausdins 10 kartu zodi "labas"
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Labas");
+        }
+
+        // ==============2 uzduotis =========================
+
+        System.out.println("==================== 2 užduotis ====================");
+
+        // Ciklas, kuris spausdina skaicius nuo 0 iki 9
+
+        int numb = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numb + " arba jei imtume... " + i);
+            numb++;
+        }
+
+
+        // ==============3 uzduotis =========================
+
+        System.out.println("==================== 3 užduotis ====================");
+
+        // Sukurkite masyvą su dešimt augalų pavadinimų
+
+
+        String [] plants = {"liepa","beržas","ažuolas", "gluosnis", "vyšnia", "slyva", "kaštonas", "klevas", "obelis", "šermukšnis"};
+
+
+        // ==============4 uzduotis =========================
+
+        System.out.println("==================== 4 užduotis ====================");
+        // Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje
+
+            for (int i =0; i < 10; i++) {
+            System.out.println(plants [i]);
+}
+
+        // ==============5 uzduotis =========================
+
+        System.out.println("==================== 5 užduotis ====================");
+        // Atspausdinkite 3čio uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju.
+        // (atvirkščias ciklas).
+
+
+
+
+        // ==============6 uzduotis =========================
+
+        System.out.println("==================== 6 užduotis ====================");
+
+    // Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
+
+// su if
+
+
+        // ==============7 uzduotis =========================
+
+        System.out.println("==================== 7 užduotis ====================");
+        //Atspausdinkite kas antrą skaičių nuo 10 iki 50. (porinius)
+        // Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite.( naudokite continue.)
+        // (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
+
+
+        // ==============8 uzduotis =========================
+
+        System.out.println("==================== 8 užduotis ====================");
+
+
+        // Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
+
+
+
+
+
+        // ==============9 uzduotis =========================
+
+        System.out.println("==================== 9 užduotis ====================");
+
+        // Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek
+        // ilgesnių nei 7 simboliai. (du skaičiavimai)
+
+
+        // ==============10 uzduotis =========================
+
+        System.out.println("==================== 10 užduotis ====================");
+
+
+
+
+
+
+
+//=====================================
+        //failo pabaiga
+
+
+    }
+}
