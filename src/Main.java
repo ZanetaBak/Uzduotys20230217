0e8d5b0d990fe32d8413aeeb93a982a64cc05af3
ZanetaB <zzanule@gmail.com>
2023-02-19 17:03:25 +0200

    isprestos uzduotys iki S-2 (neiskaitant)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -33,17 +35,17 @@ public class Main {
         // Sukurkite masyvą su dešimt augalų pavadinimų
 
 
-        String [] plants = {"liepa","beržas","ažuolas", "gluosnis", "vyšnia", "slyva", "kaštonas", "klevas", "obelis", "šermukšnis"};
-
+        String[] plants = {"liepa", "tuja", "ažuolas", "gluosnis", "vyšnia", "slyva", "kaštonas", "klevas", "obelis", "šermukšnis"};
+        System.out.println("Atsakymas: nieko spausdinti nereikia");
 
         // ==============4 uzduotis =========================
 
         System.out.println("==================== 4 užduotis ====================");
         // Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje
 
-            for (int i =0; i < 10; i++) {
-            System.out.println(plants [i]);
-}
+        for (int i = 0; i < 10; i++) {
+            System.out.println(plants[i]);
+        }
 
         // ==============5 uzduotis =========================
 
@@ -52,15 +54,23 @@ public class Main {
         // (atvirkščias ciklas).
 
 
+        for (int i = 10 - 1; i >= 0; i--) {
+            System.out.println(plants[i]);
+        }
 
 
         // ==============6 uzduotis =========================
 
         System.out.println("==================== 6 užduotis ====================");
 
-    // Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
+        // Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
 
-// su if
+
+        for (int i = 10; i < 50; i++) {
+            if (i % 2 == 0) { // jei skaicius porinis, tada spausdinti skaiciu
+                System.out.println(i);
+            }
+        }
 
 
         // ==============7 uzduotis =========================
@@ -71,6 +81,17 @@ public class Main {
         // (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
 
 
+        for (int num = 10; num < 50; num++) {
+            // jei dalinasi is 10 be liekanos, tada continue
+            if (num % 10 == 0) {
+                continue;
+            }
+            if (num % 2 == 0) { // jei skaicius porinis, tada spausdinam
+                System.out.println(num);
+            }
+        }
+
+
         // ==============8 uzduotis =========================
 
         System.out.println("==================== 8 užduotis ====================");
@@ -78,8 +99,14 @@ public class Main {
 
         // Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
 
-
-
+        int count = 0;
+        for (int i = 0; i < 20; i++) { // ar "iki 20" reiskia, kad 20 irgi turi patekti i cikla?
+            if (i % 2 == 0) { // jei skaicius porinis, tada ji suskaiciuoti
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println("Porine reiksme pasitaike tiek kartu: " + count);
 
 
         // ==============9 uzduotis =========================
@@ -90,9 +117,95 @@ public class Main {
         // ilgesnių nei 7 simboliai. (du skaičiavimai)
 
 
+        int count91 = 0;
+        for (int i = 0; i < plants.length - 1; i++) {
+            if (plants[i].length() < 5) {
+                System.out.println(plants[i]);
+                count91++;
+            }
+        }
+
+        System.out.println("Zodziai trumpesni nei 5 raides: " + count91);
+
+        int count92 = 0;
+        for (int i = 0; i < plants.length - 1; i++) {
+            if (plants[i].length() > 7) {
+                System.out.println(plants[i]);
+                count92++;
+            }
+        }
+        System.out.println("Zodziai ilgesni nei 7 raides: " + count92);
+
+
         // ==============10 uzduotis =========================
 
         System.out.println("==================== 10 užduotis ====================");
+
+        // Suskaičiuoti kiek 3čio uždavinio masyve yra žodžių ilgesnių nei 5 simboliai bet trumpesnių  nei 10 simboliai.
+        // (tarp 5 ir 10 simbolių ilgio)
+
+
+        int count10 = 0;
+        for (int i = 0; i < plants.length - 1; i++) {
+            if (plants[i].length() > 5 && plants[i].length() < 10) {
+                System.out.println(plants[i]);
+                count10++;
+            }
+        }
+
+        System.out.println("Zodziai ilgesni nei 5, bet trumpesni nei 10 raidziu: " + count10);
+
+
+        // SUNKESNES UZDUOTYS
+// ==============S-1 uzduotis =========================
+
+        System.out.println("==================== S-1 užduotis ====================");
+
+        //	Sugeneruokite 300 atsitiktinių skaičių nuo 0 iki 300, atspausdinkite juos atskirtus tarpais ir
+        //	suskaičiuokite kiek tarp jų yra didesnių už 150.
+        //	Skaičiai didesni nei 275 turi būti atspausdinti skliausteliuose” [ ] “.
+
+        int[] numbs = new int[300];
+        Random randomNumb = new Random(); // naudosiu nextInt() metoda
+        for (int i = 0; i < numbs.length; i++) {
+            numbs[i] = randomNumb.nextInt(0, 300); // uzpildo masyva atsitiktiniais skaiciais nuo 0 (isk) iki 300 (neisk)
+        }
+        System.out.println("Randomly filled arrays elements are:");
+        for (int i = 0; i < numbs.length; i++) { // atspausdinu masyvo turini vienoje eiluteje su tarpais
+            System.out.print(numbs[i] + " ");
+        }
+
+        System.out.println(" "); // jeigu nepridedu sito, tolimesni teksta spausdina toje pacioje eiluteje
+
+        // toliau skaiciuojam kiek yra didesniu uz 150
+
+        int countS1 = 0;
+        for (int i = 0; i < plants.length - 1; i++) {// "-1" nes skaiciuojam nuo 0, neatemus ciklas kartotusi 301
+            if (numbs[i] > 150) {
+                countS1++;
+            }
+        }
+        System.out.println("Didesniu uz 150 yra: " + countS1);
+
+    // didesnius nei 275 spausdinti skliausteliuose []
+
+
+        for (int i = 0; i < plants.length - 1; i++) {
+            if (numbs[i] > 275) {
+                System.out.println("[ " + numbs [i]+ " ]" + " ");
+            }
+        }
+
+    // ==============S-2 uzduotis =========================
+
+        System.out.println("==================== S-2 užduotis ====================");
+    // Vienoje eilutėje atspausdinkite visus skaičius nuo 1 iki 3000, kurie dalijasi iš 77 be liekanos.
+        // Skaičius atskirkite kableliais. Po paskutinio skaičiaus kablelio neturi būti.
+
+
+
+
+
 
 
 
